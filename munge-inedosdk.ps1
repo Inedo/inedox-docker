b11ceae4e327a9159242d73fc1e3c4d72a77ead7
Alex Papadimoulis, Inedo <apapadimoulis@inedo.com>
2023-07-09 00:02:10 -0400

    added pack/munge scripts

---
+++ munge-inedosdk.ps1
@@ -0,0 +1,15 @@
+$pkgName = "Inedo.SDK"
+$pkgProjFile= "c:\Projects\Inedo.sdk\Inedo.SDK\Inedo.SDK.csproj"
+
+$slnFile = "C:\Projects\inedox-Docker\Docker\Docker.sln"
+$projFilesToMunge = @( `
+  "C:\Projects\inedox-docker\Docker\InedoExtension\InedoExtension.csproj"
+)
+
+dotnet sln "$slnFile" add "$pkgProjFile"
+foreach ($projFile in $projFilesToMunge) {
+  dotnet remove "$projFile" package "$pkgName"
+  dotnet add $projFile reference $pkgProjFile
+}
+
+pause
