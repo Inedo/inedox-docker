8e7c9b2dc500ebff7588737c9ba8666ee585225f
Richard Hessinger <rhessinger@inedo.com>
2023-08-03 15:04:10 -0400

    #19 Added generic docker command

---
+++ Docker/InedoExtension/Operations/DockerCommandOperation.cs
@@ -0,0 +1,80 @@
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+using Inedo.Documentation;
+using Inedo.ExecutionEngine.Executer;
+using Inedo.Extensibility;
+using Inedo.Extensibility.Operations;
+using Inedo.Extensions.Docker.SuggestionProviders;
+using Inedo.Web;
+
+#nullable enable
+
+namespace Inedo.Extensions.Docker.Operations;
+
+[ScriptAlias("Execute-Command")]
+[ScriptNamespace("Docker")]
+[DisplayName("Executes a Docker Command")]
+[Description("Logs into a Docker registry and runs a Docker CLI command on a container host server.")]
+public sealed class DockerCommandOperation : DockerOperation
+{
+    [Required]
+    [ScriptAlias("Command")]
+    [DisplayName("Command")]
+    [Description("Command for the Docker CLI command")]
+    public string? Command { get; set; }
+
+    [ScriptAlias("Repository")]
+    [DisplayName("Repository")]
+    [SuggestableValue(typeof(RepositoryResourceSuggestionProvider))]
+    [DefaultValue("$DockerRepository")]
+    [Description("This is only used for logging into a Docker registry. Leave blank to not login to a Docker registry.")]
+    public string? RepositoryResourceName { get; set; }
+
+    [ScriptAlias("Arguments")]
+    [DisplayName("Arguments")]
+    [Description("Arguments for the Docker CLI command, such as --env key=value")]
+    public string? Arguments { get; set; }
+
+    [Category("Legacy")]
+    [ScriptAlias("RepositoryName")]
+    [DisplayName("Override repository name")]
+    [PlaceholderText("Do not override repository")]
+    public string? LegacyRepositoryName { get; set; }
+
+    public override async Task ExecuteAsync(IOperationExecutionContext context)
+    {
+        if (string.IsNullOrEmpty(this.Command))
+            throw new ExecutionFailureException($"A Command was not specified.");
+        
+        var repoResource = string.IsNullOrWhiteSpace(this.RepositoryResourceName) ? this.CreateRepository(context, this.RepositoryResourceName, this.LegacyRepositoryName) : null;
+
+        var client = await DockerClientEx.CreateAsync(this, context);
+
+        if (repoResource != null)
+            await client.LoginAsync(repoResource);
+        try
+        {
+            await client.DockerAsync($"{this.Command} {this.Arguments}", true);
+        }
+        finally
+        {
+            if (repoResource != null)
+                await client.LogoutAsync();
+        }
+    }
+
+    protected override ExtendedRichDescription GetDescription(IOperationConfiguration config)
+    {
+        return new ExtendedRichDescription(
+            new RichDescription(
+                "Run docker ",
+                new Hilite(config[nameof(Command)])
+            ),
+            new RichDescription(
+                "using ",
+                new Hilite(config[nameof(RepositoryResourceName)]),
+                "."
+            )
+        );
+    }
+}
--- Docker/InedoExtension/Operations/RunContainerOperation.cs
+++ Docker/InedoExtension/Operations/RunContainerOperation.cs
@@ -13,161 +13,160 @@ using Inedo.Web;
 
 #nullable enable
 
-namespace Inedo.Extensions.Docker.Operations
+namespace Inedo.Extensions.Docker.Operations;
+
+[ScriptAlias("Run-Container")]
+[ScriptNamespace("Docker")]
+[DisplayName("Run Docker Container")]
+[Description("Runs a Docker container on a container host server using a container configuration file.")]
+public sealed class RunContainerOperation : DockerOperation
 {
-    [ScriptAlias("Run-Container")]
-    [ScriptNamespace("Docker")]
-    [DisplayName("Run Docker Container")]
-    [Description("Runs a Docker container on a container host server using a container configuration file.")]
-    public sealed class RunContainerOperation : DockerOperation
+    [ScriptAlias("Repository")]
+    [ScriptAlias("Source", Obsolete = true)]
+    [DisplayName("Repository")]
+    [SuggestableValue(typeof(RepositoryResourceSuggestionProvider))]
+    [DefaultValue("$DockerRepository")]
+    public string? RepositoryResourceName { get; set; }
+    [ScriptAlias("Tag")]
+    [DefaultValue("$DockerTag")]
+    public string? Tag { get; set; }
+
+    [Category("Run Config")]
+    [DisplayName("Docker Run Config")]
+    [ScriptAlias("DockerRunConfig")]
+    [ScriptAlias("ConfigFileName", Obsolete = true)]
+    [SuggestableValue(typeof(ConfigurationSuggestionProvider))]
+    [DefaultValue("DockerRun")]
+    public string? DockerRunConfig { get; set; }
+    [Category("Run Config")]
+    [DisplayName("Docker Run Config instance")]
+    [ScriptAlias("DockerRunConfigInstance")]
+    [ScriptAlias("ConfigFileInstanceName", Obsolete = true)]
+    [SuggestableValue(typeof(ConfigurationInstanceSuggestionProvider))]
+    [DefaultValue("$PipelineStageName")]
+    public string? DockerRunConfigInstance { get; set; }
+
+    [Category("Legacy")]
+    [ScriptAlias("RepositoryName")]
+    [DisplayName("Override repository name")]
+    [PlaceholderText("Do not override repository")]
+    public string? LegacyRepositoryName { get; set; }
+
+    [Category("Advanced")]
+    [DisplayName("Repository URL")]
+    [ScriptAlias("RepositoryUrl")]
+    [PlaceholderText("ex: mcr.microsoft.com/mssql/server")]
+    [Description("Repository URL is a public Docker Repository to pull an image without the need to login.  This overrides the Repository parameter.  ex: mcr.microsoft.com/mssql/server")]
+    public string? RepositoryUrl { get; set; }
+
+    [Category("Advanced")]
+    [DisplayName("Restart policy")]
+    [ScriptAlias("Restart")]
+    [DefaultValue("unless-stopped")]
+    [SuggestableValue("no", "on-failure", "always", "unless-stopped")]
+    public string? RestartPolicy { get; set; }
+    [Category("Advanced")]
+    [DisplayName("Container name")]
+    [ScriptAlias("ContainerName")]
+    [ScriptAlias("Container", Obsolete = true)]
+    [PlaceholderText("use repository name")]
+    public string? ContainerName { get; set; }
+    [Category("Advanced")]
+    [DisplayName("Run in background")]
+    [ScriptAlias("RunInBackground")]
+    [DefaultValue(true)]
+    public bool? RunInBackground { get; set; }
+    [Category("Advanced")]
+    [DisplayName("Remove the container on exit")]
+    [ScriptAlias("RemoveOnExit")]
+    public bool RemoveOnExit { get; set; }
+    [Category("Advanced")]
+    [ScriptAlias("AdditionalArguments")]
+    [DisplayName("Addtional arguments")]
+    [Description("Additional arguments for the docker CLI exec command, such as --env key=value")]
+    public string? AdditionalArguments { get; set; }
+
+    public override async Task ExecuteAsync(IOperationExecutionContext context)
     {
-        [ScriptAlias("Repository")]
-        [ScriptAlias("Source", Obsolete = true)]
-        [DisplayName("Repository")]
-        [SuggestableValue(typeof(RepositoryResourceSuggestionProvider))]
-        [DefaultValue("$DockerRepository")]
-        public string? RepositoryResourceName { get; set; }
-        [ScriptAlias("Tag")]
-        [DefaultValue("$DockerTag")]
-        public string? Tag { get; set; }
+        if (string.IsNullOrEmpty(this.RepositoryResourceName) && string.IsNullOrWhiteSpace(this.RepositoryUrl))
+            throw new ExecutionFailureException($"A RepositoryResourceName or a RepositoryUrl was not specified.");
+        if (string.IsNullOrEmpty(this.Tag))
+            throw new ExecutionFailureException($"A Tag was not specified.");
+        if (!string.IsNullOrEmpty(this.DockerRunConfig) && string.IsNullOrEmpty(this.DockerRunConfigInstance))
+            throw new ExecutionFailureException($"An Instance is required when specifying a Docker Run Config.");
 
-        [Category("Run Config")]
-        [DisplayName("Docker Run Config")]
-        [ScriptAlias("DockerRunConfig")]
-        [ScriptAlias("ConfigFileName", Obsolete = true)]
-        [SuggestableValue(typeof(ConfigurationSuggestionProvider))]
-        [DefaultValue("DockerRun")]
-        public string? DockerRunConfig { get; set; }
-        [Category("Run Config")]
-        [DisplayName("Docker Run Config instance")]
-        [ScriptAlias("DockerRunConfigInstance")]
-        [ScriptAlias("ConfigFileInstanceName", Obsolete = true)]
-        [SuggestableValue(typeof(ConfigurationInstanceSuggestionProvider))]
-        [DefaultValue("$PipelineStageName")]
-        public string? DockerRunConfigInstance { get; set; }
+        var repoResource = string.IsNullOrWhiteSpace(this.RepositoryUrl) ? this.CreateRepository(context, this.RepositoryResourceName, this.LegacyRepositoryName) : null;
+        var repository = AH.NullIf(this.RepositoryUrl, string.Empty) ?? repoResource?.GetRepository(context);
 
-        [Category("Legacy")]
-        [ScriptAlias("RepositoryName")]
-        [DisplayName("Override repository name")]
-        [PlaceholderText("Do not override repository")]
-        public string? LegacyRepositoryName { get; set; }
+        if (string.IsNullOrEmpty(repository))
+            throw new ExecutionFailureException($"Docker repository \"{this.RepositoryResourceName}\" has an unexpected name.");
 
-        [Category("Advanced")]
-        [DisplayName("Repository URL")]
-        [ScriptAlias("RepositoryUrl")]
-        [PlaceholderText("ex: mcr.microsoft.com/mssql/server")]
-        [Description("Repository URL is a public Docker Repository to pull an image without the need to login.  This overrides the Repository parameter.  ex: mcr.microsoft.com/mssql/server")]
-        public string? RepositoryUrl { get; set; }
+        if (string.IsNullOrEmpty(this.ContainerName))
+            this.ContainerName = repository.Split('/').Last();
 
-        [Category("Advanced")]
-        [DisplayName("Restart policy")]
-        [ScriptAlias("Restart")]
-        [DefaultValue("unless-stopped")]
-        [SuggestableValue("no", "on-failure", "always", "unless-stopped")]
-        public string? RestartPolicy { get; set; }
-        [Category("Advanced")]
-        [DisplayName("Container name")]
-        [ScriptAlias("ContainerName")]
-        [ScriptAlias("Container", Obsolete = true)]
-        [PlaceholderText("use repository name")]
-        public string? ContainerName { get; set; }
-        [Category("Advanced")]
-        [DisplayName("Run in background")]
-        [ScriptAlias("RunInBackground")]
-        [DefaultValue(true)]
-        public bool? RunInBackground { get; set; }
-        [Category("Advanced")]
-        [DisplayName("Remove the container on exit")]
-        [ScriptAlias("RemoveOnExit")]
-        public bool RemoveOnExit { get; set; }
-        [Category("Advanced")]
-        [ScriptAlias("AdditionalArguments")]
-        [DisplayName("Addtional arguments")]
-        [Description("Additional arguments for the docker CLI exec command, such as --env key=value")]
-        public string? AdditionalArguments { get; set; }
+        var dockerRunText = await getDockerRunTextAsync();
 
-        public override async Task ExecuteAsync(IOperationExecutionContext context)
+        var repositoryAndTag = $"{repository}:{this.Tag}";
+
+        var client = await DockerClientEx.CreateAsync(this, context);
+
+        if(repoResource != null)
+            await client.LoginAsync(repoResource);
+        try
         {
-            if (string.IsNullOrEmpty(this.RepositoryResourceName) && string.IsNullOrWhiteSpace(this.RepositoryUrl))
-                throw new ExecutionFailureException($"A RepositoryResourceName or a RepositoryUrl was not specified.");
-            if (string.IsNullOrEmpty(this.Tag))
-                throw new ExecutionFailureException($"A Tag was not specified.");
-            if (!string.IsNullOrEmpty(this.DockerRunConfig) && string.IsNullOrEmpty(this.DockerRunConfigInstance))
-                throw new ExecutionFailureException($"An Instance is required when specifying a Docker Run Config.");
+            await client.DockerAsync($"pull {client.EscapeArg(repositoryAndTag)}");
 
-            var repoResource = string.IsNullOrWhiteSpace(this.RepositoryUrl) ? this.CreateRepository(context, this.RepositoryResourceName, this.LegacyRepositoryName) : null;
-            var repository = AH.NullIf(this.RepositoryUrl, string.Empty) ?? repoResource?.GetRepository(context);
+            var runArgs = new StringBuilder($"run --name {client.EscapeArg(this.ContainerName)}");
+            if (!string.IsNullOrEmpty(dockerRunText))
+                runArgs.Append($" {dockerRunText}");
+            if (this.RemoveOnExit)
+                runArgs.Append(" --rm");
+            if (this.RunInBackground ?? true)
+                runArgs.Append(" -d");
+            if (!string.IsNullOrWhiteSpace(AdditionalArguments))
+                runArgs.Append($" {this.AdditionalArguments}");
+            runArgs.Append($" {client.EscapeArg(repositoryAndTag)}");
 
-            if (string.IsNullOrEmpty(repository))
-                throw new ExecutionFailureException($"Docker repository \"{this.RepositoryResourceName}\" has an unexpected name.");
-
-            if (string.IsNullOrEmpty(this.ContainerName))
-                this.ContainerName = repository.Split('/').Last();
-
-            var dockerRunText = await getDockerRunTextAsync();
-
-            var repositoryAndTag = $"{repository}:{this.Tag}";
-
-            var client = await DockerClientEx.CreateAsync(this, context);
-
-            if(repoResource != null)
-                await client.LoginAsync(repoResource);
-            try
-            {
-                await client.DockerAsync($"pull {client.EscapeArg(repositoryAndTag)}");
-
-                var runArgs = new StringBuilder($"run --name {client.EscapeArg(this.ContainerName)}");
-                if (!string.IsNullOrEmpty(dockerRunText))
-                    runArgs.Append($" {dockerRunText}");
-                if (this.RemoveOnExit)
-                    runArgs.Append(" --rm");
-                if (this.RunInBackground ?? true)
-                    runArgs.Append(" -d");
-                if (!string.IsNullOrWhiteSpace(AdditionalArguments))
-                    runArgs.Append($" {this.AdditionalArguments}");
-                runArgs.Append($" {client.EscapeArg(repositoryAndTag)}");
-
-                await client.DockerAsync(runArgs.ToString(), true);
-            }
-            finally
-            {
-                if (repoResource != null)
-                    await client.LogoutAsync();
-            }
-
-            async Task<string?> getDockerRunTextAsync()
-            {
-                if (string.IsNullOrEmpty(this.DockerRunConfig))
-                    return null;
-
-                var deployer = (await context.TryGetServiceAsync<IConfigurationFileDeployer>())
-                    ?? throw new ExecutionFailureException("Configuration files are not supported in this context.");
-
-                using var writer = new StringWriter();
-                if (!await deployer.WriteAsync(writer, this.DockerRunConfig, this.DockerRunConfigInstance, this))
-                    throw new ExecutionFailureException("Error reading Docker Run Config.");
-
-                return Regex.Replace(writer.ToString(), @"\r?\n", " ");
-            }
+            await client.DockerAsync(runArgs.ToString(), true);
+        }
+        finally
+        {
+            if (repoResource != null)
+                await client.LogoutAsync();
         }
 
-        protected override ExtendedRichDescription GetDescription(IOperationConfiguration config)
+        async Task<string?> getDockerRunTextAsync()
         {
-            return new ExtendedRichDescription(
-                new RichDescription(
-                    "Run ",
-                    new Hilite(config[nameof(RepositoryResourceName)] + ":" + config[nameof(Tag)]),
-                    " Docker image"
-                ),
+            if (string.IsNullOrEmpty(this.DockerRunConfig))
+                return null;
 
-                new RichDescription(
-                    "using ",
-                    new Hilite(config[nameof(DockerRunConfigInstance)]),
-                    " instance of ",
-                    new Hilite(config[nameof(DockerRunConfig)]),
-                    "."
-                )
-            );
+            var deployer = (await context.TryGetServiceAsync<IConfigurationFileDeployer>())
+                ?? throw new ExecutionFailureException("Configuration files are not supported in this context.");
+
+            using var writer = new StringWriter();
+            if (!await deployer.WriteAsync(writer, this.DockerRunConfig, this.DockerRunConfigInstance, this))
+                throw new ExecutionFailureException("Error reading Docker Run Config.");
+
+            return Regex.Replace(writer.ToString(), @"\r?\n", " ");
         }
+    }
+
+    protected override ExtendedRichDescription GetDescription(IOperationConfiguration config)
+    {
+        return new ExtendedRichDescription(
+            new RichDescription(
+                "Run ",
+                new Hilite(config[nameof(RepositoryResourceName)] + ":" + config[nameof(Tag)]),
+                " Docker image"
+            ),
+
+            new RichDescription(
+                "using ",
+                new Hilite(config[nameof(DockerRunConfigInstance)]),
+                " instance of ",
+                new Hilite(config[nameof(DockerRunConfig)]),
+                "."
+            )
+        );
     }
 }
