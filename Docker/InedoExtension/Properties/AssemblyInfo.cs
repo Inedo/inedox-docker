87157a7df0372b543c489b167eb145495c9f6ddd
gdivis <gdivis@inedo.com>
2024-10-23 22:42:07 -0400

    Sdk 3.1 (#24)
    * update to sdk 3.1
    
    * update sdk

--- Docker/InedoExtension/Properties/AssemblyInfo.cs
+++ Docker/InedoExtension/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using Inedo.Extensibility;
 [assembly: AssemblyTitle("Docker")]
 [assembly: AssemblyDescription("Provides operations that act on Docker and Docker Compose.")]
 [assembly: AssemblyCompany("Inedo, LLC.")]
-[assembly: AssemblyCopyright("Copyright © Inedo 2018")]
+[assembly: AssemblyCopyright("Copyright © Inedo 2024")]
 [assembly: AssemblyProduct("any")]
 
 // Not for ProGet
@@ -12,5 +12,5 @@ using Inedo.Extensibility;
 
 [assembly: ScriptNamespace("Docker")]
 
-[assembly: AssemblyVersion("2.4.0")]
-[assembly: AssemblyFileVersion("2.4.0")]
+[assembly: AssemblyVersion("3.1.0")]
+[assembly: AssemblyFileVersion("3.1.0")]
