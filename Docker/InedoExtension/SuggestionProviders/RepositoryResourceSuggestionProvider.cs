62719721be4a9c29e6ec8387fdad0927c1e928cd
Greg Divis <gdivis@inedo.com>
2026-03-18 12:51:40 -0400

    Update SDK to 4.0 and remove not implemented suggestion providers

--- Docker/InedoExtension/SuggestionProviders/RepositoryResourceSuggestionProvider.cs
+++ Docker/InedoExtension/SuggestionProviders/RepositoryResourceSuggestionProvider.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 using Inedo.Extensibility;
 using Inedo.Extensibility.SecureResources;
 using Inedo.Extensions.SecureResources;
@@ -10,10 +10,10 @@ namespace Inedo.Extensions.Docker.SuggestionProviders;
 
 internal sealed class RepositoryResourceSuggestionProvider : ISuggestionProvider
 {
-    public Task<IEnumerable<string>> GetSuggestionsAsync(IComponentConfiguration config)
+    public IAsyncEnumerable<string> GetSuggestionsAsync(IComponentConfiguration config, CancellationToken cancellationToken)
     {
-        return Task.FromResult(from resource in SDK.GetSecureResources(config.EditorContext as IResourceResolutionContext)
-                               where resource.InstanceType == typeof(DockerRepository)
-                               select resource.Name);
+        return (from resource in SDK.GetSecureResources(config.EditorContext as IResourceResolutionContext)
+                where resource.InstanceType == typeof(DockerRepository)
+                select resource.Name).ToAsyncEnumerable();
     }
 }
